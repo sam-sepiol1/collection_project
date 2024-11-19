848c95c015fefa1ff06757410b82ddaf9932b7b9
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2024-11-19 13:35:37 +0100

    project done

--- index.js
+++ index.js
@@ -1,4 +1,54 @@
-// Data  
-const collection = require('./src/collection.json');
+// Data
+const artists = await fetch("./src/collection.json")
+	.then((response) => response.json())
+	.catch((error) => console.error("Error loading JSON:", error));
 
 
+// DOM
+const collection = document.getElementById("collection");
+
+
+
+
+// function
+
+artists.forEach((artist) => {
+	const content = document.createElement("article");
+	content.classList.add("collection-item");
+
+  let genresHTML = '';
+  artist.genre.forEach((genre) => {
+      genresHTML += `<p class="card__genre-item">${genre}</p> `;
+  });
+	
+  content.innerHTML = `
+                <div class="card">
+                    <div class="card__image">
+                        <picture>
+                            <img src="${artist.picture}" alt="${artist.name}">
+                        </picture>
+                    </div>
+                    <div class="card__genre">
+                        ${genresHTML}
+                    </div>
+                    <div class="card__text">
+                        <h2 class="card__title">${artist.name}</h2>
+                        <p class="card__description">${artist.description}</p>
+                        <hr>
+                        <div class="card__links">
+                            <a href="${artist.youtube}">
+                                <img src="./assets/youtube.png" alt="youtube">
+                            </a>
+                            <a href="${artist.spotify}">
+                                <img src="./assets/spotify.png" alt="spotify">
+                            </a>
+                            <a href="${artist.instagram}">
+                                <img src="./assets/instagram.png" alt="instagram">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+  `;
+
+  collection.appendChild(content);
+});
